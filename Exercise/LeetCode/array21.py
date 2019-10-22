a7671cacd81566de14ca4f4e62fa42dfe94a3aa9
contr4l <30452426+contr4l@users.noreply.github.com>
2019-10-23 01:02:54 +0800

    Add header comments

--- Exercise/LeetCode/array21.py
+++ Exercise/LeetCode/array21.py
@@ -1,3 +1,16 @@
+# -*- coding: utf-8 -*-
+# @Author: contr4l
+# @Email: china_go.tj@163.com
+# @Date:   2019-10-23 00:44:23
+# @Last Modified by:   contr4l
+# @Last Modified time: 2019-10-23 01:02:26
+
+'''
+给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+
+不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+'''
+
 class Solution(object):
     def removeDuplicates(self, nums):
         """
