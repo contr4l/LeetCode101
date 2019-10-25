78cb3860fdc98b8c3fbceb8d268beb26852c832e
contr4l <china_go.tj@163.com>
2019-10-25 11:23:21 +0800

    Problem 2 and 3.

---
+++ Exercise/LeetCode/array22.py
@@ -0,0 +1,18 @@
+"""
+Topic: 买卖股票的最佳时机 II
+给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+设计一个算法来计算你所能获取的最大利润。你可以尽可能地完成更多的交易（多次买卖一支股票）。
+注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+"""
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if len(prices) <= 1:
+            return 0
+        res = 0
+        tmp = prices[0]
+        for i in prices[1:]:
+            if i > tmp:
+                res = res + (i - tmp)
+            tmp = i
+        return res
